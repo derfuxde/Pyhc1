bd8f9a5b776546ae5a4fc5882b4f1f81d303f0e7
derfuxde <109156763+derfuxde@users.noreply.github.com>
2024-07-22 10:46:28 +0000

    Commit

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import json
 import random
+
+
+def generate_random_number():
+    """Generates a random number and writes it to a JSON file."""
+    random_number = random.randint(1, 100)
+    with open('random_number.json', 'w') as f:
+        json.dump({'number': random_number}, f)
+
