5488e4cde7823db5ab485f1bec5cdc9e168f61ce
derfuxde <109156763+derfuxde@users.noreply.github.com>
2024-07-23 12:07:25 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import random
 
+var r_number = random.randint(1, 100)
 
 def generate_random_number():
     """Generates a random number and writes it to a JSON file."""
@@ -14,4 +15,13 @@ def isnub(number, isnumber):
     else:
         print("Number is false" , number, "should be " , isnumber)
     
+   
+def garv():
+   V = random.randint(10,50)
+   F = random.randint(V,51)
+   print(F , "is on this pos)
+   while r_number != F:
+      r_number = random.randint(1, 100)
+   
+   
     
